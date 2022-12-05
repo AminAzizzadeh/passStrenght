9acd407ea85f82c55b6e7164adad61cb53adb78b
AminAzizzadeh <aminazizzadeh13@gmail.com>
2022-12-05 15:22:23 +0330

    V1.1

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const smallWord = document.querySelector(".smallWordTrue");
 const capitalWord = document.querySelector(".capitalWordTrue");
 const number = document.querySelector(".numberTrue");
 const char = document.querySelector(".charTrue");
-const result = document.querySelector(".passStrength");
+const result = document.querySelector(".passStrenght");
 let finalResult = 0;
 let trues = 0;
 
@@ -18,32 +18,32 @@ function smallWordCheck() {
     finalResult -= smallWordScore;
     smallWordScore = 0;
 
-    x += pass.value.search("a"); 
-    x += pass.value.search("b"); 
-    x += pass.value.search("c"); 
-    x += pass.value.search("d"); 
-    x += pass.value.search("e"); 
-    x += pass.value.search("f"); 
-    x += pass.value.search("g"); 
-    x += pass.value.search("h"); 
-    x += pass.value.search("i"); 
-    x += pass.value.search("j"); 
-    x += pass.value.search("k"); 
-    x += pass.value.search("l"); 
-    x += pass.value.search("m"); 
-    x += pass.value.search("n"); 
-    x += pass.value.search("o"); 
-    x += pass.value.search("p"); 
-    x += pass.value.search("q"); 
-    x += pass.value.search("r"); 
-    x += pass.value.search("s"); 
-    x += pass.value.search("t"); 
-    x += pass.value.search("u"); 
-    x += pass.value.search("v"); 
-    x += pass.value.search("w"); 
-    x += pass.value.search("x"); 
-    x += pass.value.search("y"); 
-    x += pass.value.search("z"); 
+    x += pass.value.search("a");
+    x += pass.value.search("b");
+    x += pass.value.search("c");
+    x += pass.value.search("d");
+    x += pass.value.search("e");
+    x += pass.value.search("f");
+    x += pass.value.search("g");
+    x += pass.value.search("h");
+    x += pass.value.search("i");
+    x += pass.value.search("j");
+    x += pass.value.search("k");
+    x += pass.value.search("l");
+    x += pass.value.search("m");
+    x += pass.value.search("n");
+    x += pass.value.search("o");
+    x += pass.value.search("p");
+    x += pass.value.search("q");
+    x += pass.value.search("r");
+    x += pass.value.search("s");
+    x += pass.value.search("t");
+    x += pass.value.search("u");
+    x += pass.value.search("v");
+    x += pass.value.search("w");
+    x += pass.value.search("x");
+    x += pass.value.search("y");
+    x += pass.value.search("z");
 
     x += 25;
 
@@ -51,11 +51,11 @@ function smallWordCheck() {
         smallWordScore = 15;
         smallWordTrue = true;
         smallWord.classList.add("pluginEnable");
+        trues++;
         x = 0;
     }
-    if(x < 0){
+    if (x < 0) {
         smallWord.classList.remove("pluginEnable");
-        trues++;
         x++;
     }
     finalResult += smallWordScore;
@@ -69,32 +69,32 @@ function captalWordCheck() {
     finalResult -= capitalWordScore;
     capitalWordScore = 0;
 
-    y += pass.value.search("A"); 
-    y += pass.value.search("B"); 
-    y += pass.value.search("C"); 
-    y += pass.value.search("D"); 
-    y += pass.value.search("E"); 
-    y += pass.value.search("F"); 
-    y += pass.value.search("G"); 
-    y += pass.value.search("H"); 
-    y += pass.value.search("I"); 
-    y += pass.value.search("J"); 
-    y += pass.value.search("K"); 
-    y += pass.value.search("L"); 
-    y += pass.value.search("M"); 
-    y += pass.value.search("N"); 
-    y += pass.value.search("O"); 
-    y += pass.value.search("P"); 
-    y += pass.value.search("Q"); 
-    y += pass.value.search("R"); 
-    y += pass.value.search("S"); 
-    y += pass.value.search("T"); 
-    y += pass.value.search("U"); 
-    y += pass.value.search("V"); 
-    y += pass.value.search("W"); 
-    y += pass.value.search("y"); 
-    y += pass.value.search("Y"); 
-    y += pass.value.search("Z"); 
+    y += pass.value.search("A");
+    y += pass.value.search("B");
+    y += pass.value.search("C");
+    y += pass.value.search("D");
+    y += pass.value.search("E");
+    y += pass.value.search("F");
+    y += pass.value.search("G");
+    y += pass.value.search("H");
+    y += pass.value.search("I");
+    y += pass.value.search("J");
+    y += pass.value.search("K");
+    y += pass.value.search("L");
+    y += pass.value.search("M");
+    y += pass.value.search("N");
+    y += pass.value.search("O");
+    y += pass.value.search("P");
+    y += pass.value.search("Q");
+    y += pass.value.search("R");
+    y += pass.value.search("S");
+    y += pass.value.search("T");
+    y += pass.value.search("U");
+    y += pass.value.search("V");
+    y += pass.value.search("W");
+    y += pass.value.search("y");
+    y += pass.value.search("Y");
+    y += pass.value.search("Z");
 
     y += 25;
 
@@ -105,7 +105,7 @@ function captalWordCheck() {
         trues++;
         y = 0;
     }
-    if(y < 0){
+    if (y < 0) {
         capitalWord.classList.remove("pluginEnable");
         y++;
     }
@@ -140,7 +140,7 @@ function numberCheck() {
         trues++;
         z = 0;
     }
-    if(z < 0){
+    if (z < 0) {
         number.classList.remove("pluginEnable");
         z++;
     }
@@ -175,14 +175,14 @@ function charCheck() {
         trues++;
         w = 0;
     }
-    if(w < 0){
+    if (w < 0) {
         char.classList.remove("pluginEnable");
     }
     finalResult += charScore;
 }
 
 function changeClass() {
-    result.classList.remove("w-25", "w-50", "w-75", "w-100", "red", "orange", "yellow", "green");
+    result.classList.remove("red", "orange", "yellow", "green");
 }
 
 let lenghtScore = 0;
@@ -195,7 +195,7 @@ pass.addEventListener("input", function () {
     showPass.textContent = pass.value;
     finalResult -= lenghtScore;
 
-    if (data <= 7) {
+    if (data < 8) {
         //incorrect
         lenghtScore = 0;
         lenghtTrue = false;
@@ -204,25 +204,21 @@ pass.addEventListener("input", function () {
         //weak
         lenghtScore = 10;
         lenghtTrue = true;
-        trues++;
     }
     else if (data >= 13 && data <= 16) {
         //normal
         lenghtScore = 20;
         lenghtTrue = true;
-        trues++;
     }
     else if (data >= 17 && data <= 20) {
         //good
         lenghtScore = 30;
         lenghtTrue = true;
-        trues++;
     }
     else {
         //exellent
         lenghtScore = 40;
         lenghtTrue = true;
-        trues++;
     }
     finalResult += lenghtScore;
 });
@@ -235,34 +231,32 @@ pass.addEventListener("input", numberCheck);
 
 pass.addEventListener("input", charCheck);
 
-pass.addEventListener("input", function(){
-    console.log(finalResult)
+pass.addEventListener("input", function () {
 
-    if(trues >= 1 || lenghtTrue == true){
+    if (trues >= 1 || lenghtTrue == true) {
         
-        if (finalResult < 20) {
+        document.getElementById("widthchanger").style.width = finalResult + "%";
+        result.textContent = finalResult + "%";
+
+        if (finalResult < 15) {
             changeClass();
             result.textContent = "";
         }
-        else if (finalResult > 20 && finalResult <= 40) {
+        else if (finalResult >= 15 && finalResult <= 35) {
             changeClass();
-            result.classList.add("red", "w-25");
-            result.textContent = "Weak";
+            result.classList.add("red");
         }
-        else if (finalResult > 40 && finalResult <= 60) {
+        else if (finalResult > 35 && finalResult <= 60) {
             changeClass();
-            result.classList.add("orange", "w-50");
-            result.textContent = "Normal";
+            result.classList.add("orange");
         }
-        else if (finalResult > 60 && finalResult <=80) {
+        else if (finalResult > 60 && finalResult <= 80) {
             changeClass();
-            result.classList.add("yellow", "w-75");
-            result.textContent = "Good";
+            result.classList.add("yellow");
         }
         else if (finalResult > 80) {
             changeClass();
-            result.classList.add("green","w-100");
-            result.textContent = "Excellent";
+            result.classList.add("green");
         }
     }
 });
